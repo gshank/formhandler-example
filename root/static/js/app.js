aebb574824264fe655257a5de9981c371d243b40
Dimitar Petrov <dimitar.petrov@bayerninvest.de>
2012-11-28 16:30:53 +0100

    Add table sorter

--- root/static/js/app.js
+++ root/static/js/app.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  // enable tablesorter
+  $('.tablesorter').tablesorter();
+
   // jquery validation
   if ($('#validation_json').length > 0) {
     var validationJSON = JSON.parse(decodeURIComponent($('#validation_json').val()));
@@ -7,4 +10,5 @@ $(document).ready(function() {
         messages: validationJSON.messages
     });
   }
+
 });
