f68a02ee7d40ffd616104c8c65785d9d5159b956
Dimitar Petrov <dimitar.petrov@bayerninvest.de>
2012-11-28 13:30:05 +0100

    Add * to all required fields

--- lib/MyApp/Form/Base.pm
+++ lib/MyApp/Form/Base.pm
@@ -12,4 +12,11 @@ has_field validation_json => ( type => 'Hidden',  element_attr => { disabled => 
 
 sub default_validation_json { shift->as_escaped_json }
 
+sub html_attributes {
+    my ( $self, $field, $type, $attr ) = @_;
+    $field->{label} .= ' *' if $type eq 'label' && $field->{required};
+    return $attr;
+}
+
+
 1;
